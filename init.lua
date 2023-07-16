3c4d27c6de16d5929103d09cda70675995f42bde
JohnnyGoodNews <johannes.stammer@gmail.com>
2023-07-16 09:41:31 +0200

    theme

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "github_dark_high_contrast",
+  -- colorscheme = "astrodark",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
